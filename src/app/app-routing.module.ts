beeb874e5b013905c81c7ad727aa580ee15ed16c
Cristian Ortega <crisortegamunoz@gmail.com>
2023-05-31 20:38:39 -0400

    Avance en carrera de frontend angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
 
-const routes: Routes = [];
+import { NotFoundComponent } from './not-found/not-found.component';
+
+import { CustomPreloadService } from './services/custom-preload.service';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./website/website.module').then(m => m.WebsiteModule),
+    data: {
+      preload: true
+    }
+  },
+  {
+    path: 'cms',
+    loadChildren: () => import('./cms/cms.module').then(m => m.CmsModule)
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: CustomPreloadService
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,95 +1,46 @@
 import { Component } from '@angular/core';
-import { Product } from './product.model';
+
+import { UsersService } from './services/users.service';
+import { FilesService } from './services/files.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<router-outlet></router-outlet>',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  widthImg = 10;
-  name = 'Cristian';
-  age = 30;
-  img = 'https://www.w3schools.com/howto/img_avatar.png';
-  btnDisabled = true;
-  person = {
-    name: 'Cristian',
-    age: 30,
-    avatar: 'https://www.w3schools.com/howto/img_avatar.png'
+  imgRta = '';
+
+  constructor(
+    private usersService: UsersService,
+    private filesService: FilesService
+  ) {}
+
+  createUser() {
+    this.usersService.create({
+      name: 'Sebas',
+      email: 'sebas@mail.com',
+      password: '1212'
+    })
+    .subscribe(rta => {
+      console.log(rta);
+    });
   }
 
-  register = {
-    name: '',
-    email: '',
-    password: ''
+  downloadPdf() {
+    this.filesService.getFile('my.pdf', 'https://young-sands-07814.herokuapp.com/api/files/dummy.pdf', 'application/pdf')
+    .subscribe()
   }
 
-  names: string[] = ['Cristian', 'Camila', 'Sergio']
-  newName = '';
-  products: Product[] = [
-    {
-      name: 'EL mejor juguete',
-      price: 565,
-      image: './assets/images/toy.jpg',
-      category: 'all',
-    },
-    {
-      name: 'Bicicleta casi nueva',
-      price: 356,
-      image: './assets/images/bike.jpg'
-    },
-    {
-      name: 'Colleción de albumnes',
-      price: 34,
-      image: './assets/images/album.jpg'
-    },
-    {
-      name: 'Mis libros',
-      price: 23,
-      image: './assets/images/books.jpg'
-    },
-    {
-      name: 'Casa para perro',
-      price: 34,
-      image: './assets/images/house.jpg'
-    },
-    {
-      name: 'Gafas',
-      price: 3434,
-      image: './assets/images/glasses.jpg'
+  onUpload(event: Event) {
+    const element = event.target as HTMLInputElement;
+    const file = element.files?.item(0);
+    if (file) {
+      this.filesService.uploadFile(file)
+      .subscribe(rta => {
+        this.imgRta = rta.location;
+      });
     }
-  ]
-  toggleButton() {
-    this.btnDisabled = !this.btnDisabled;
-  }
 
-  increaseAge() {
-    this.person.age += 1;
   }
-
-  onScroll(event: Event) {
-    const element = event.target as HTMLElement;
-    console.log(element.scrollTop)
-  }
-
-  changeName(event: Event) {
-    const input = event.target as HTMLInputElement;
-    this.person.name = input.value;
-  }
-
-  addName() {
-    if (this.newName !== '') {
-      this.names.push(this.newName);
-      this.newName = '';
-    }
-  }
-
-  onDelete(index: number) {
-    this.names.splice(index, 1);
-  }
-
-  onRegister() {
-    console.log(this.register);
-  }
-
 }
